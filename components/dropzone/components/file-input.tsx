eb9ec2bb0803cd4484254df1cfcc284cb1ed76b9
iNikolas <realkolos1@gmail.com>
2024-03-15 16:34:22 +0200

    parse file draft

--- components/dropzone/components/file-input.tsx
+++ components/dropzone/components/file-input.tsx
@@ -1,24 +1,18 @@
 import React from "react";
 
 import { supportedFormats } from "@/config";
-
-import { isValidFileType } from "../utils";
+import { parseFile } from "@/utils";
 
 export function FileInput() {
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-
-    if (!file || !isValidFileType(file)) {
-      return;
-    }
-
-    console.log(file.name);
-  };
   return (
     <label htmlFor="fileInput" className="absolute inset-0">
       <span className="sr-only">Оберіть файли</span>
       <input
-        onChange={handleFileChange}
+        onChange={(e) => {
+          parseFile(e.target.files?.[0])
+            .then((data) => console.log(data))
+            .catch((error) => console.log(error));
+        }}
         type="file"
         accept={supportedFormats.map((format) => `.${format}`).join(", ")}
         className="absolute inset-0 opacity-0 w-full h-full z-20 cursor-pointer"
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,6 +1,52 @@
+import Papa from "papaparse";
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import { supportedFormats } from "@/config";
+
+function parseFileExtension(name?: string): string | null {
+  if (!name) {
+    return null;
+  }
+
+  const parts = name.split(".");
+
+  if (parts.length === 1) {
+    return null;
+  }
+
+  if (parts.length === 2 && parts[0] === "") {
+    return null;
+  }
+
+  return parts.pop() ?? null;
+}
+
+function isValidFileType(file?: File) {
+  const fileExtension = parseFileExtension(file?.name);
+
+  if (!fileExtension) {
+    return false;
+  }
+
+  return supportedFormats.includes(fileExtension);
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function parseFile(file?: File): Promise<unknown> {
+  return new Promise((resolve, reject) => {
+    if (!file || !isValidFileType(file)) {
+      reject(new Error("File type is invalid"));
+      return;
+    }
+
+    Papa.parse(file, {
+      delimiter: ",",
+      complete: (result) => resolve(result),
+      error: (error) => reject(error),
+    });
+  });
 }
