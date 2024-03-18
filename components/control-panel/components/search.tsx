10b6f5dcc270b6abc755e8d4ddee0b46628717bf
iNikolas <realkolos1@gmail.com>
2024-03-18 14:29:01 +0200

    recipe name search field refactoring

--- components/control-panel/components/search.tsx
+++ components/control-panel/components/search.tsx
@@ -7,12 +7,12 @@ import { reportFiltersModel } from "@/stores";
 
 export function Search() {
   const search = useUnit(reportFiltersModel.$searchInput);
-  const searchInputChanged = useUnit(reportFiltersModel.searchInputChanged);
+  const searchChanged = useUnit(reportFiltersModel.searchInputChanged);
 
   return (
     <input
       value={search}
-      onChange={(e) => searchInputChanged(e.target.value)}
+      onChange={(e) => searchChanged(e.target.value)}
       type="text"
       placeholder="Пошук за назвою рецепта"
       className="input input-bordered w-auto"
